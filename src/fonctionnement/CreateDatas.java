415440d1ed4f6c6dbc5a8600c5d5822fe3d2eef9
Thomas-Lienard <t.lienard92@gmail.com>
2015-01-27 17:54:56 +0100

    datas

--- src/fonctionnement/CreateDatas.java
+++ src/fonctionnement/CreateDatas.java
@@ -38,14 +38,32 @@ public class CreateDatas {
 		System.out.println("INSERT INTO stations VALUES ('"+8+"', '11 rue des Aborigenes', '20', '0');");
 		System.out.println("INSERT INTO stations VALUES ('"+9+"', '56 Boulevard des Olives Vertes ', '20', '0');");
 		System.out.println("INSERT INTO stations VALUES ('"+10+"', 'Centre du Bois de Boulogne', '20', '0');");
-		//		
-		for(i=0; i<20; i++){
-			int annee = (int) (2014+(Math.random()*(2015-2014)));
-			int mois = (int) (1+(Math.random()*(12-1)));
-			int jour = (int) (1+(Math.random()*(28-1)));
-			System.out.println("INSERT INTO valeur VALUES ('"+i+"', '50 rue De Gaulle', '20', '0');");
+		//valeurs	
+		for(i=0; i<30; i++){
+			int station = (int) (1+(Math.random()*(10-1)));
+			String etat;
+			if(i%3==0){
+				etat = "Vplus";
+				System.out.println("INSERT INTO valeur VALUES ('"+i+"', '"+station+"', '"+etat+"', to_date('08:00:00','HH24:MI:SS'),to_date('11:00:00','HH24:MI:SS') );");
+			}else if(i%5==0){
+				etat = "Vmoins";
+				System.out.println("INSERT INTO valeur VALUES ('"+i+"', '"+station+"', '"+etat+"', to_date('17:00:00','HH24:MI:SS'),to_date('19:00:00','HH24:MI:SS') );");
+			}else{
+				etat = "Vnul";
+				System.out.println("INSERT INTO valeur VALUES ('"+i+"', '"+station+"', '"+etat+"', null, null);");
+			}
 		}
-
+		//bornettes
+		for(i=0; i<30; i++){
+			int station = (int) (1+(Math.random()*(10-1)));
+			int velo = (int) (1+(Math.random()*(59-1)));
+			//System.out.println("INSERT INTO bornettes VALUES ('"+i+"', '"+velo+"', '"+station+"', 'Libre' );");
+			System.out.println("INSERT INTO bornettes VALUES ('"+i+"', null, '"+station+"', 'Libre' );");			
+		}
+		//clients
+		for(i=1; i<=20; i++){
+		//	System.out.println("INSERT INTO clients VALUES ('"+i+"', null, '"+station+"', 'Libre' );");
+		}
 	}
 
 }
