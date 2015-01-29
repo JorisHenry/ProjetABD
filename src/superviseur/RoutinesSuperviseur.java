0ce48df4903ac6ff725f614464b2565e18d92ad4
joris <joris@joris-GE60-0NC-GE60-0ND>
2015-01-29 08:32:47 +0100

    Datas OK

--- src/superviseur/RoutinesSuperviseur.java
+++ src/superviseur/RoutinesSuperviseur.java
@@ -6,13 +6,35 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 
-/**
- * Requête en rapport avec le superviseur
- * 
- * @param conn : Connexion à la BDD
- * @param noDossier : le numero du dossier
- * @throws SQLException : En cas d'erreur d'accès à la BDD
- */
 public class RoutinesSuperviseur {
-	
+	/**
+	 * Etant donn� un achat, affiche les informations concernant l�ensemble des places achet�es.
+	 * 
+	 * @param conn : Connexion � la BDD
+	 * @param IdStation : Identifiant de la station
+	 * @throws SQLException : En cas d'erreur d'acc�s � la BDD
+	 */
+	public static void ConsulterVelosStation(Connection conn, int IdStation) throws SQLException {
+		// Get a statement from the connection
+		Statement stmt = conn.createStatement();
+
+		// Execute the query
+		ResultSet rs = stmt
+				.executeQuery("SELECT IdS, IdVelo "
+						+ "FROM possede natural join accueil "
+						+ "WHERE IdS = " + IdStation);
+
+		// Loop through the result set
+		if (rs.next()) {
+			System.out.println("Informations sur les Velos de la station " + IdStation + " :"+"\n");
+			do {	
+				System.out.println("Numero du velo : "+ rs.getString("IdVelo")+ "\n");
+			} while (rs.next());
+		} else {
+			System.out.println("Aucun velo n'est enregistré dans la station " + IdStation + ".");
+		}
+		// Close the result set, statement and the connection
+		rs.close();
+		stmt.close();
+	}
 }
